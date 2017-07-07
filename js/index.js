5a073a0062c1e378bb37e02b9bb3ace520948e60
Sarah Veness <sarahhveness@gmail.com>
2017-07-07 14:31:31 -0700

    basic jquery added. quotes appear on page and tweet button works to allow to tweet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+$(document).ready(function() {
+
+  function getQuote() {
+    var url= 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+
+    $.getJSON(url, function(data) {
+      $(".quote").html(data.quoteText);
+      $(".author").html(data.quoteAuthor);
+    });
+  };
+
+
+  $("#tweetQuote").on("click", function() {
+    var tweetQuote= $(".quote").text() + " by -" + $(".author").text();
+    window.open("https://twitter.com/intent/tweet?text=" + tweetQuote);
+  });
+
+  $("#newQuote").on("click", function() {
+    getQuote();
+  });
+
+});
